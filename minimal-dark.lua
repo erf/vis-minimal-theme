f6262bf2f3d06e2b12a2e8b4c7995153be86ff2b
Erlend Lind Madsen <erlendf80@gmail.com>
2022-01-15 01:33:18 +0100

    dark: rename

--- minimal-dark.lua
+++ minimal-dark.lua
@@ -1,63 +1,56 @@
 -- vis-minimal-theme (https://github.com/erf/vis-minimal-theme)
 -- by Erlend Lind Madsen
--- based on base16-grayscale-dark by Petr Shevtsov (https://github.com/pshevtsov/base16-vis/themes/base16-grayscale-dark)
 
 local lexers = vis.lexers
 
-local colors = {
-	['base00'] = '#000000',
-	['base01'] = '#888888',
-	['base02'] = '#ffffff',
-	['base03'] = '#444444',
-	['base04'] = '#ffffff',
-	['base05'] = '#ffffff',
-	['base06'] = '#ffffff',
-	['base07'] = '#ffffff',
-	['base08'] = '#ffffff',
-	['base09'] = '#bbbbbb',
-	['base0A'] = '#ffffff',
-	['base0B'] = '#bbbbbb',
-	['base0C'] = '#ffffff',
-	['base0D'] = '#ffffff',
-	['base0E'] = '#aaaaaa',
-	['base0F'] = '#ffffff',
+local col = {
+	BACK = '#000000',
+	FORE = '#ffffff',
+	STAT_BACK = '#444444',
+	STAT_FORE = '#bbbbbb',
+	COMMENT = '#484848',
+	VAL = '#bbbbbb',
+	base04 = '#0000ff',
+	ERROR = '#ffffff',
+	TYPE = '#ffffff',
+	VAR = '#444444',
+	ID = '#ffffff',
+	KEY = '#eeeeee',
+	base0F = '#ffff00',
 }
 
-lexers.colors = colors
+lexers.col = col
 
-local fg = ',fore:'..colors.base05..','
-local bg = ',back:'..colors.base00..','
+local fg = ',fore:'..col.FORE..','
+local bg = ',back:'..col.BACK..','
 
 lexers.STYLE_DEFAULT = bg..fg
 lexers.STYLE_NOTHING = bg
-lexers.STYLE_CLASS = 'fore:'..colors.base0A
-lexers.STYLE_COMMENT = 'fore:'..colors.base03..',italics'
-lexers.STYLE_CONSTANT = 'fore:'..colors.base09
-lexers.STYLE_DEFINITION = 'fore:'..colors.base0E
-lexers.STYLE_ERROR = 'fore:'..colors.base08..',italics'
-lexers.STYLE_FUNCTION = 'fore:'..colors.base0D
-lexers.STYLE_KEYWORD = 'fore:'..colors.base0E
-lexers.STYLE_LABEL = 'fore:'..colors.base0A
-lexers.STYLE_NUMBER = 'fore:'..colors.base09
-lexers.STYLE_OPERATOR = 'fore:'..colors.base05
-lexers.STYLE_REGEX = 'fore:'..colors.base0C
-lexers.STYLE_STRING = 'fore:'..colors.base0B
-lexers.STYLE_PREPROCESSOR = 'fore:'..colors.base0A
-lexers.STYLE_TAG = 'fore:'..colors.base0A
-lexers.STYLE_TYPE = 'fore:'..colors.base0A
-lexers.STYLE_VARIABLE = 'fore:'..colors.base0D
-lexers.STYLE_WHITESPACE = 'fore:'..colors.base02
-lexers.STYLE_EMBEDDED = 'fore:'..colors.base0F
-lexers.STYLE_IDENTIFIER = 'fore:'..colors.base08
+lexers.STYLE_CLASS = 'fore:'..col.TYPE
+lexers.STYLE_COMMENT = 'fore:'..col.COMMENT..',italics'
+lexers.STYLE_CONSTANT = 'fore:'..col.VAL
+lexers.STYLE_DEFINITION = 'fore:'..col.KEY
+lexers.STYLE_ERROR = 'fore:'..col.ERROR..',italics'
+lexers.STYLE_FUNCTION = 'fore:'..col.VAR
+lexers.STYLE_KEYWORD = 'fore:'..col.KEY
+lexers.STYLE_LABEL = 'fore:'..col.TYPE
+lexers.STYLE_NUMBER = 'fore:'..col.VAL
+lexers.STYLE_OPERATOR = 'fore:'..col.FORE
+lexers.STYLE_REGEX = 'fore:'..col.VAL
+lexers.STYLE_STRING = 'fore:'..col.VAL
+lexers.STYLE_PREPROCESSOR = 'fore:'..col.TYPE
+lexers.STYLE_TAG = 'fore:'..col.TYPE
+lexers.STYLE_TYPE = 'fore:'..col.TYPE
+lexers.STYLE_VARIABLE = 'fore:'..col.VAR
+lexers.STYLE_EMBEDDED = 'fore:'..col.base0F
+lexers.STYLE_IDENTIFIER = 'fore:'..col.ID
 
-lexers.STYLE_LINENUMBER = 'fore:'..colors.base02..',back:'..colors.base00
-lexers.STYLE_CURSOR = 'fore:'..colors.base00..',back:'..colors.base05
-lexers.STYLE_CURSOR_PRIMARY = 'fore:'..colors.base00..',back:'..colors.base05
-lexers.STYLE_CURSOR_LINE = 'back:'..colors.base01
-lexers.STYLE_COLOR_COLUMN = 'back:'..colors.base01
-lexers.STYLE_SELECTION = 'back:'..colors.base02
-lexers.STYLE_STATUS = 'fore:'..colors.base04..',back:'..colors.base01
-lexers.STYLE_STATUS_FOCUSED = 'fore:'..colors.base09..',back:'..colors.base01
+lexers.STYLE_CURSOR = 'fore:'..col.BACK..',back:'..col.FORE
+lexers.STYLE_CURSOR_PRIMARY = 'fore:'..col.BACK..',back:'..col.FORE
+lexers.STYLE_CURSOR_LINE = 'back:'..col.STAT_BACK
+lexers.STYLE_COLOR_COLUMN = 'back:'..col.STAT_BACK
+lexers.STYLE_STATUS = 'fore:'..col.base04..',back:'..col.STAT_BACK
+lexers.STYLE_STATUS_FOCUSED = 'fore:'..col.STAT_FORE..',back:'..col.STAT_BACK
 lexers.STYLE_SEPARATOR = lexers.STYLE_DEFAULT
-lexers.STYLE_INFO = 'fore:default,back:default,bold'
+lexers.STYLE_INFO = 'fore:default, back:default,bold'
 lexers.STYLE_EOF = ''
