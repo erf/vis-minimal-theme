28cf0a665cc11a6c4b4f53823a075f2ff0afa56e
Erlend Lind Madsen <erlendf80@gmail.com>
2022-08-03 21:41:48 +0200

    rename | set cursor color to 888

--- clear.lua
+++ clear.lua
@@ -1,14 +1,14 @@
 -- vis-minimal-theme (https://github.com/erf/vis-minimal-theme)
 -- clear by Erlend Lind Madsen
 -- uses a transparent background and the terminal foreground
--- syntax highlighting using font styles by https://github.com/qiu-x
+-- font style syntax highlighting by https://github.com/qiu-x
 
-local white2 = '#989898'
-local clear0 = '#00000000'
+local grey0 = '#888888'
+local clear = '#00000000'
 
 local lexers = vis.lexers
 
-lexers.STYLE_DEFAULT ='back:'..clear0
+lexers.STYLE_DEFAULT ='back:'..clear
 lexers.STYLE_NOTHING = ''
 lexers.STYLE_CLASS = 'fore:,bold'
 lexers.STYLE_COMMENT = 'fore:,italics'
@@ -27,18 +27,18 @@ lexers.STYLE_TAG = 'fore:,underline'
 lexers.STYLE_TYPE = 'fore:,bold'
 lexers.STYLE_VARIABLE = 'fore:'
 lexers.STYLE_WHITESPACE = ''
-lexers.STYLE_EMBEDDED = 'back:'..clear0
+lexers.STYLE_EMBEDDED = 'back:'..clear
 lexers.STYLE_IDENTIFIER = 'fore:'
 
 lexers.STYLE_LINENUMBER = 'fore:'
 lexers.STYLE_LINENUMBER_CURSOR = lexers.STYLE_LINENUMBER
-lexers.STYLE_CURSOR = 'back:'..white2
+lexers.STYLE_CURSOR = 'back:'..grey0
 lexers.STYLE_CURSOR_PRIMARY = lexers.STYLE_CURSOR
 lexers.STYLE_CURSOR_LINE = 'underlined'
-lexers.STYLE_COLOR_COLUMN = 'back:'..white2
-lexers.STYLE_SELECTION = 'back:'..white2
+lexers.STYLE_COLOR_COLUMN = 'back:'..grey0
+lexers.STYLE_SELECTION = 'back:'..grey0
 lexers.STYLE_STATUS = 'reverse'
-lexers.STYLE_STATUS_FOCUSED = 'back:'..white2
+lexers.STYLE_STATUS_FOCUSED = 'back:'..grey0
 lexers.STYLE_SEPARATOR = lexers.STYLE_DEFAULT
 lexers.STYLE_INFO = 'fore:default,back:default'
 lexers.STYLE_EOF = ''
